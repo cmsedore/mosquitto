47b587415deb10dbabde6130e616655ac3a46c36
Chris Sedore <cmsedore@gmail.com>
2022-04-12 09:57:48 +0000

    fix mount point path

--- lib/send_publish.c
+++ lib/send_publish.c
@@ -65,6 +65,19 @@ int send__publish(struct mosquitto *mosq, uint16_t mid, const char *topic, uint3
 	}
 
 #ifdef WITH_BROKER
+	if(mosq->listener && mosq->listener->mount_point){
+		char *mp=mosquitto__malloc(512);
+		parse_mount_point(mosq,&mp,"");
+		len = strlen(mp);
+		log__printf(mosq,MOSQ_LOG_DEBUG,">>>> mountp %s, mp=%s\n",mosq->listener->mount_point,mp);
+		free(mp);
+		if(len < strlen(topic)){
+			topic += len;
+		}else{
+			/* Invalid topic string. Should never happen, but silently swallow the message anyway. */
+			return MOSQ_ERR_SUCCESS;
+		}
+	}
 #ifdef WITH_BRIDGE
 	if(mosq->bridge && mosq->bridge->topics && mosq->bridge->topic_remapping){
 		for(i=0; i<mosq->bridge->topic_count; i++){
